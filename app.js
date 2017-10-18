c86ae6ddcabd6d01d918c86fdf59995b6594130b
Mitchell M <mitchellm1618@gmail.com>
2017-10-18 16:19:01 -0700

    added while loop

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ if (arizona.toLowerCase() === 'y' || arizona.toLowerCase() === 'yes') {
 var yoga = prompt(user + ', based off what you learned last time, do you think I\'m one to enjoy 105+ Farenheit hatha yoga?');
 console.log('yogi? ', yoga);
 
-// Q3
+// 3rd question validation if user enters y or yes
 if (yoga.toLowerCase() === 'y' || yoga.toLowerCase() === 'yes') {
   alert('Nice! I tried to trick you there, but you\'re apparently not one to be fooled so easily. Moving on...');
 
@@ -46,16 +46,55 @@ if (yoga.toLowerCase() === 'y' || yoga.toLowerCase() === 'yes') {
   alert('Ha! Tricked you. I might be avoidant of the lack of water for extended periods of time, but I actually love heat in controlled settings. Plus, with 90 minutes of that stuff, you\'re basically the source of your own personal lake. Moving on...');
 }
 
+// 4th question
 var iceAxe = prompt(user + ', how about whether I\'ve self-arrested down snow faces on mountains with an ice axe?');
 console.log('...self-arrest with ice axe? ', iceAxe);
 
+// 4th question validation if user enters y or yes
 if (iceAxe.toLowerCase() === 'y' || iceAxe.toLowerCase() === 'yes') {
   alert('OK I know I\'m getting lazy. But good guess nonetheless, although it was much less exciting than it sounds. Final question!');
+
+// 4th question validation if user enters anything else
 } else {
   alert('OK I know I\'m getting lazy. I definitely have, although it was much less exciting than it sounds. Final question!');
 }
 
+// 5th question
 var entity = prompt(user + ', last but not least, am I a digital entity?');
 console.log('entity: ', entity);
 
+// 5th question response regardless of input
 alert('Whoops another trick. I have no idea! Do you? c; Thanks for taking some time to play the get to know me game! I\'m happy to return the favor. For now, here\'s a little extra info before we part ways. Goodbye!! :D <3');
+
+alert ('I\'d like to test your 6th sense now... Tune into my vibration as well as you are able. I\'m going to give you four guesses to determine which day of the month I was born. It was in August if that makes it easier to feel. Relax, I\'ll give you some hints too :]');
+
+// declaring variables for upcoming loop
+var birthday;
+var counter = 0;
+var flag = false;
+
+// while loop to give user 4 chances
+while (birthday !== 13 && counter < 4) {
+  birthday = parseInt(prompt('What day of the month was I born?'));
+  counter++;
+  console.log('guessed: ', birthday);
+  console.log('guess #: ', counter);
+
+  // if loop that determines whether the guess was higher/lower or correct (which breaks loop)
+  if (birthday === 13) {
+    flag = true;
+    break;
+  } else if (birthday < 13) {
+    alert('Nope! Too low c:');
+  } else if (birthday > 13) {
+    alert('Nope! Too high c:');
+  }
+}
+
+if (flag === true) {
+  alert('Congrats, you got it! I\'m no guru but your 3rd eye might be open!');
+} else {
+  alert('Out of guesses! Nice try, your odds were pretty awful anyways.');
+}
+
+// 7th question
